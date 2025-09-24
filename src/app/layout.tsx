c7f82f0e9864cb25a3f45b6a37e8e65a9e2a851c
NicoPiovesan <niccolopiovesan04@gmail.com>
2025-09-24 13:30:29 +0200

    fixing favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
     <Suspense fallback={<p>Loading…</p>}>
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" type="image/x-icon" href="/logoSalcai.ico" />
+      <link rel="icon" type="image/x-icon" href="/favicon.ico" />
       </head>
       <body className={inter.className}>
         <LangProvider>
